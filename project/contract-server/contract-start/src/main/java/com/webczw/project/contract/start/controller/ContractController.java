87b44433d402ab3e6e45f4a6c0b663f1ab3ad910
webczw <webczw@163.com>
2017-06-17 23:32:21 +0800

    update contracts

---
+++ project/contract-server/contract-start/src/main/java/com/webczw/project/contract/start/controller/ContractController.java
@@ -0,0 +1,14 @@
+package com.webczw.project.contract.start.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("contracts")
+public class ContractController {
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String index() {
+		return "test";
+	}
+}
